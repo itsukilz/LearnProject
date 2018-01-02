e39c29b134600f1c7c5474c961aa40f411655a00
Lu <tomomedific@gmail.com>
2018-01-02 09:39:55 +0800

    string-3

--- java/Test.java
+++ java/Test.java
@@ -1,38 +1,27 @@
 public class Test{
-public static boolean sameStarChar(String str) {
-  int start = 0;
-  int found;
-  while(true){
-    found = str.indexOf("*",start);
-    if(ifsame(str,found,start)!=0)
-    	return false;
-   	else{
-   		if(found+1>str.length()-1) break;
-   		else start = found+1;
-   	}
-  
+public static int countYZ(String str) {
+  int count = 0;
+  for(int i = 0; i<str.length(); i++){
+    if(ifyz(str.charAt(i))){
+      if(i==str.length()-1)
+        count++;
+      else{
+        if(i>=1 && !Character.isLetter(str.charAt(i-1)))
+        count++;
+      }
+    }
   }
-  return true;
+  return count;
+}
+
+public static boolean ifyz(char x){
+  if( x== 'y' ||x== 'z' || x == 'Y' ||x == 'Z')
+    return true;
+  else 
+    return false;
 }
     
-
-public static int ifsame(String str, int found, int start){
-	int flag = 0;
-	if(found == -1){
-		if(start==0) flag = 0;
-		else  flag = 0;
-	}
-	else{
-		if(found-1<0 || found+1>str.length()-1) flag=0;
-		else{
-			if(str.charAt(found-1) != str.charAt(found+1)) flag = 1;
-			else flag = 0;
-		}
-	}
-	return flag;
-}
-
     public static void main(String[] args){
-        System.out.println(sameStarChar("12*2*3*"));
+        System.out.println(countYZ(""));
         }
     }
