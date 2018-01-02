b3786302f9f03599cd224772f13bf6f43d5f36e4
af <tomomedific@gmail.com>
2018-01-02 10:40:19 +0800

    简单更新

--- java/Test.java
+++ java/Test.java
@@ -1,5 +1,28 @@
-public class Test{
-    public static void main(String[] args){
-        System.out.println((1+2.236)/2);
-        }
-    }
+public class Test {
+public static String deFront(String str) {    
+  int l = str.length();
+  if(l==0) return "";
+  else if(l==1) {
+    if(str.equals("a")) return "a";
+    else return "";
+  }
+  else{
+    String f = (str.substring(0,1).equals("a")) ? "a":"";
+    String s = (str.substring(1,2).equals("b")) ? "b":"";
+    String last = (l>2) ? str.substring(2) : "";
+    return f+s+last;
+  }
+}
+
+
+
+	public static void main(String[] args) {
+		// int[] k = new int[]{6, 7, 2, 6,6,7,9};
+		System.out.println(deFront("blueasdfa"));
+		System.out.println(deFront("abad"));
+		System.out.println(deFront("a"));
+		System.out.println(deFront(""));
+
+
+	}
+}
