9e2f4effd376f6a7ff0595e39f104b2e2aa6a591
af <tomomedific@gmail.com>
2018-01-22 12:29:08 +0800

    Recursion-1

--- java/Test.java
+++ java/Test.java
@@ -1,32 +1,27 @@
 public class Test{
-public static int maxMirror(int[] nums) {
-  int len = nums.length;
-  int count = 0;
-  int max = 0;
-  int i,j;
-  for(int start = 0; start<len; start++){
-    for(int reverseStart = len-1; reverseStart >= 0; reverseStart--){
-      if(nums[start] == nums[reverseStart]){
-        i = start;
-        j = reverseStart;
-
-        while(i<len && j>=0 &&nums[i]==nums[j]){
-          count++;
-          i++;
-          j--;
-        }
-        max = Math.max(max,count);
-        count = 0;
-      }
-    }
+public static int count8(int n) {
+  System.out.println(n);
+  int flag;
+  if(n<10) {
+    if(n==8) return 1;
+    else return 0;
   }
-  return max;
+  else if(n>9 && n<100){
+    if(n%10==8){
+      if(n/10==8) flag=2;
+      else flag= 1;
+    }
+    else flag= 0;
+    return flag+count8((n/10));
+  }
+  else return count8(n%100)+count8(n/10);
   
 }
 
+
     
 public static void main(String[] args){
-  int[] k = new int[]{1, 2, 3, 8, 9, 3, 2, 1};
-      System.out.println(maxMirror(k));
-        }
+      System.out.println(count8(8088));
+        
     }
+  }
